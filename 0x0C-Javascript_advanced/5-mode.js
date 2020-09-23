b68037d5af77281532ef14789ee454f32ea42676
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-09-24 00:50:11 +0100

    still under work and understanding

---
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -0,0 +1,20 @@
+function changeMode (size, weight, transform, background, color) {
+
+    document.body.style.fontSize = size + "px";
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+}
+
+function main () {
+    const spooky = (function () { return changeMode(9, "bold", "uppercase", "pink", "green") });
+    const darkMode = (function () { return changeMode(12, "bold", "capitalize", "black", "white") });
+    const screamMode = (function () {return changeMode(12, "normal", "lowercase", "white", "black") });
+    document.getElementsByTagName("button")[0].addEventListener("click", spooky());
+    document.getElementsByTagName("button")[1].addEventListener("click", darkMode());
+    document.getElementsByTagName("button")[2].addEventListener("click", function () {
+        document.getElementsByTagName("button")[2].style.backgroundColor = "black";
+    });
+}
+main()
