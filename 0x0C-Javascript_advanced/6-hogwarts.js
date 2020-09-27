720610139ea9d3191fa53067ae56b39647bebf88
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-09-27 21:50:19 +0100

    6-hogwarts.js

--- 0x0C-Javascript_advanced/6-hogwarts.js
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -3,4 +3,39 @@ class studentHogwarts {
         this.privateScore = 0;
         this.name = null;
     }
+
+    changeScoreBy (points) {
+       this.privateScore += points;
+    }
+
+    setName (newName) {
+        this.name = newName;
+    }
+
+    rewardStudent () {
+        this.changeScoreBy(1);
+    }
+
+    penalizeStudent () {
+        this.changeScoreBy(-1);
+    }
+
+    getScore () {
+        return this.name + ': ' + this.privateScore
+    }
 }
+
+let harry = new studentHogwarts();
+harry.name = 'Harry';
+for (let i = 0; i < 4; ++i) {
+    harry.rewardStudent();
+}
+console.log(harry.getScore())
+
+let draco = new studentHogwarts();
+draco.name = 'Draco';
+draco.rewardStudent();
+for (let i = 0; i < 3; ++i) {
+    draco.penalizeStudent();
+}
+console.log(draco.getScore());
