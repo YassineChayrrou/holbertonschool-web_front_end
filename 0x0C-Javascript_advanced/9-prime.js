b7f095fbdfaea870f470e09ba01767a039201ba8
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-09-28 12:43:13 +0100

    9-prime.js

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,21 @@
+let countPrimeNumbers = function () {
+    const isPrime = (num) => {
+        for(let i = 2; i < num; ++i)
+          if(num % i === 0) return false;
+        return true;
+    }
+    
+    let numberOfPrimes = 0;
+    for (let i = 2; i < 100; ++i) {
+        if (isPrime(i) === true) {
+            numberOfPrimes += 1;
+        }
+    }
+    return numberOfPrimes
+}
+
+//perfomance calculation
+const t0 = performance.now();
+countPrimeNumbers();
+const t1 = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
